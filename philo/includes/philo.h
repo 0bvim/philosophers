0615178ab445a339cec13ad5891e1a0689cd709f
nivicius <niviciusdev@gmail.com>
2024-02-24 01:54:18 -0300

    feat: main with validations
    basic input validations to handle different ac nbr

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,18 +6,29 @@
 /*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 22:43:36 by vde-frei          #+#    #+#             */
-/*   Updated: 2024/02/24 01:36:59 by vde-frei         ###   ########.fr       */
+/*   Updated: 2024/02/24 01:44:55 by vde-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
+/**
+ * needed for project
+ */
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
 # include <unistd.h>
-# include <sys/tyme.h>
+# include <sys/time.h>
+
+/**
+ * for definitions and types
+ */
+#include <stdbool.h>
+#include <stddef.h>
+#include <limits.h>
+#include <inttypes.h>
 
 typedef struct	s_time
 {
--- philo/src/main/main.c
+++ philo/src/main/main.c
@@ -6,19 +6,32 @@
 /*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 01:27:29 by vde-frei          #+#    #+#             */
-/*   Updated: 2024/02/24 01:32:05 by vde-frei         ###   ########.fr       */
+/*   Updated: 2024/02/24 01:53:26 by vde-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/philo.h"
 
+void	no_philo_must_eat(int ac, char **av)
+{
+	printf("(no_philo_must_eat)%d %s\n", ac, *av);
+}
+
+void	philo_must_eat(int ac, char **av)
+{
+	printf("(philo_must_eat)%d %s\n", ac, *av);
+}
+
 int main(int ac, char **av)
 {
-	if (5 != ac || 6 != ac)
-		return (EXIT_FAILURE);
 	if (5 == ac)
 		no_philo_must_eat(ac, av);
-	else
+	else if (6 == ac)
 		philo_must_eat(ac, av);
+	else
+	{
+		printf("(Wrong arguments)%d\n", ac);
+		return (EXIT_FAILURE);
+	}
 	return (EXIT_SUCCESS);
 }
