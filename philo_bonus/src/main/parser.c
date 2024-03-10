ad30191915368fc8e1642c12c460cb774727c4f4
nivicius <niviciusdev@gmail.com>
2024-03-09 23:04:32 -0300

    feat: init philos and semaphores funcitons

--- philo_bonus/src/main/parser.c
+++ philo_bonus/src/main/parser.c
@@ -6,16 +6,47 @@
 /*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 23:46:20 by vde-frei          #+#    #+#             */
-/*   Updated: 2024/03/09 20:59:02 by vde-frei         ###   ########.fr       */
+/*   Updated: 2024/03/09 23:04:15 by vde-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_bonus.h"
+#include <semaphore.h>
+
+int	ft_strcpy(char *dst, const char *src)
+{
+	int	i;
+
+	i = -1;
+	while (src[++i])
+		dst[i] = src[i];
+	dst[i] = 0;
+	return (i);
+}
+
+char	*make_semaphore_name(char const *base, char *buffer, int position)
+{
+	int	i;
+
+	i = ft_strcpy(buffer, base);
+	while (position > 0)
+	{
+		buffer[i++] = (position % 10) + '0';
+		position /= 10;
+	}
+	buffer[i] = 0;
+	return (buffer);
+}
+
+sem_t	*ft_sem_open(char const *name, int value)
+{
+	return (sem_open(name, O_CREAT | O_EXCL, 0644, value));
+}
 
 static void	philo_init(t_table *table)
 {
 	int		i;
-	char	semaphores[250];
+	char	semaphore[250];
 
 	i = -1;
 	while (++i < table->ph_nb)
@@ -25,8 +56,38 @@ static void	philo_init(t_table *table)
 		table->philo[i].left_fork = i;
 		table->philo[i].right_fork = (i + 1) % table->ph_nb;
 		table->philo[i].table = table;
-		//TODO: make function to build semaphores name
+		make_semaphore_name(SEM_PHILO, (char*)semaphore, i);
+		sem_unlink(semaphore);
+		table->philo[i].mutex = ft_sem_open(semaphore, 0);
+		if (table->philo[i].mutex == SEM_FAILED)
+			error_exit("Fail to open semaphore.");
+		make_semaphore_name(SEM_PHILOEAT, (char*)semaphore, i);
+		sem_unlink(semaphore);
+		table->philo[i].eat_count_mtx = ft_sem_open(semaphore, 0);
+		if (table->philo[i].eat_count_mtx == SEM_FAILED)
+			error_exit("Fail to open semaphore.");
 	}
+}
+
+void	init_semaphores(t_table *table)
+{
+	sem_unlink(SEM_FORK);
+	sem_unlink(SEM_WRITE);
+	sem_unlink(SEM_DEAD);
+	sem_unlink(SEM_DEADW);
+
+	table->fork_mtx = ft_sem_open(SEM_FORK, table->ph_nb);
+	if (table->fork_mtx == SEM_FAILED)
+		error_exit("fail in fork semaphore creation.");
+	table->write_mtx = ft_sem_open(SEM_WRITE, 1);
+	if (table->write_mtx == SEM_FAILED)
+		error_exit("fail in write semaphore creation.");
+	table->have_died_mtx = ft_sem_open(SEM_DEAD, 0);
+	if (table->have_died_mtx == SEM_FAILED)
+		error_exit("fail in have died semaphore creation.");
+	table->dead_write_mtx = ft_sem_open(SEM_DEADW, 1);
+	if (table->dead_write_mtx == SEM_FAILED)
+		error_exit("fail in dead write semaphore creation.");
 }
 
 void	parser_input(t_table *table, char **av)
@@ -43,6 +104,6 @@ void	parser_input(t_table *table, char **av)
 		table->max_meals = -1;
 	table->fork_mtx = NULL;
 	table->philo = (t_philo*)safe_malloc(sizeof(*(table->philo)) * table->ph_nb);
-	//TODO: init philos
-	//TODO: init semaphores
+	philo_init(table);
+	init_semaphores(table);
 }
